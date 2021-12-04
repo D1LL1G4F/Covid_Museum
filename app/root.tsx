f6b3cb248ede739affef7c34c61fa0837a01fcc0
Noro <bodnar@inqool.cz>
2021-12-04 20:39:28 +0100

    WIP trying to figure out how to separate navbar from content

--- app/root.tsx
+++ app/root.tsx
@@ -1,8 +1,11 @@
 import { LiveReload } from 'remix';
 import Layout, { LayoutColumn } from '@kiwicom/orbit-components/lib/Layout';
 import { useContext } from 'react';
-import Navbar from './components/Navbar';
+import Stack from '@kiwicom/orbit-components/lib/Stack';
+import Button from '@kiwicom/orbit-components/lib/primitives/ButtonPrimitive';
+import TextLink from '@kiwicom/orbit-components/lib/TextLink';
 import StylesContext from './components/StylesContext';
+import Navbar from './components/Navbar';
 
 export default function Root() {
   const styles = useContext(StylesContext);
@@ -20,11 +23,15 @@ export default function Root() {
         <title>Covid Museum</title>
       </head>
       <body>
-        <Navbar />
-        <Layout type="Booking">
-          <LayoutColumn>Column1</LayoutColumn>
-          <LayoutColumn>Column2</LayoutColumn>
-        </Layout>
+        <Stack direction="column">
+          <Navbar />
+          <Layout type="Booking">
+            <LayoutColumn>Column1</LayoutColumn>
+            <LayoutColumn>Column2</LayoutColumn>
+          </Layout>
+          <Button>My Button</Button>
+          <TextLink>Hello World!</TextLink>
+        </Stack>
 
         {process.env.NODE_ENV === 'development' ? <LiveReload /> : null}
       </body>
