67424c5eac44e49d7746b8d4c626c13d4ed65141
matej.knazik <matej.knazik@kiwi.com>
2021-12-04 21:05:09 +0100

    feat(layout): place content and header into layout

--- app/root.tsx
+++ app/root.tsx
@@ -1,11 +1,22 @@
 import { LiveReload } from 'remix';
 import Layout, { LayoutColumn } from '@kiwicom/orbit-components/lib/Layout';
 import { useContext } from 'react';
-import Stack from '@kiwicom/orbit-components/lib/Stack';
-import Button from '@kiwicom/orbit-components/lib/primitives/ButtonPrimitive';
 import TextLink from '@kiwicom/orbit-components/lib/TextLink';
+import styled from 'styled-components';
+import Stack from '@kiwicom/orbit-components/lib/Stack';
+import Button from '@kiwicom/orbit-components/lib/Button';
+import Grid from '@kiwicom/orbit-components/lib/utils/Grid';
 import StylesContext from './components/StylesContext';
 import Navbar from './components/Navbar';
+
+const NavContainer = styled.header`
+  z-index: 99;
+  position: relative;
+`;
+
+const OverlordGrid = styled(Grid)`
+  min-height: 100vh;
+`;
 
 export default function Root() {
   const styles = useContext(StylesContext);
@@ -23,15 +34,21 @@ export default function Root() {
         <title>Covid Museum</title>
       </head>
       <body>
-        <Stack direction="column">
-          <Navbar />
-          <Layout type="Booking">
-            <LayoutColumn>Column1</LayoutColumn>
-            <LayoutColumn>Column2</LayoutColumn>
+        <OverlordGrid rows="64px 1fr">
+          <NavContainer>
+            <Navbar />
+          </NavContainer>
+
+          <Layout type="MMB">
+            <LayoutColumn>
+              {/* TODO: HERE goes the content */}
+              <Stack>
+                <Button>My Button</Button>
+                <TextLink>Hello World!</TextLink>
+              </Stack>
+            </LayoutColumn>
           </Layout>
-          <Button>My Button</Button>
-          <TextLink>Hello World!</TextLink>
-        </Stack>
+        </OverlordGrid>
 
         {process.env.NODE_ENV === 'development' ? <LiveReload /> : null}
       </body>
