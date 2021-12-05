30cb721c633314be5bc26d3264677503e98866c8
matej.knazik <matej.knazik@kiwi.com>
2021-12-05 19:01:34 +0100

    refactor: refactor for index page

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,5 +1,42 @@
-import MainPage from '../components/MainPage';
+import Grid from '@kiwicom/orbit-components/lib/utils/Grid';
+import GalleryObject from '../components/GalleryObject';
 
-const IndexRoute = () => <MainPage />;
+const Images = [
+  {
+    title: 'Black-figure Aryballos: Two heraldic bearded sirens with woman',
+    dated: 'c. 600 BCE-575 BCE',
+    department: 'Department of Ancient and Byzantine Art & Numismatics',
+    division: 'Asian and Mediterranean Art',
+    period: 'Archaic period',
+    imageUrl: 'https://nrs.harvard.edu/urn-3:HUAM:767068',
+  },
+  {
+    title: 'Collection of Beads and Amulets',
+    dated: '1st Millennium BCE-1st Millennium CE',
+    department: 'Department of Ancient and Byzantine Art & Numismatics',
+    division: 'Asian and Mediterranean Art',
+    period: 'Late Period to Roman',
+    imageUrl: 'https://nrs.harvard.edu/urn-3:HUAM:INV028585_dynmc',
+  },
+  {
+    title: 'Lid or Bell',
+    dated: 'mid 1st millennium BCE-mid 1st millennium CE',
+    department: 'Department of Ancient and Byzantine Art & Numismatics',
+    division: 'Asian and Mediterranean Art',
+    period: 'Roman period',
+    imageUrl: 'https://nrs.harvard.edu/urn-3:HUAM:LEG251164_dynmc',
+  },
+];
+
+const IndexRoute = () => (
+  <Grid>
+    {Images.map((image, index) => (
+      <GalleryObject
+        image={image}
+        direction={index % 2 === 0 ? 'row' : 'row-reverse'}
+      />
+    ))}
+  </Grid>
+);
 
 export default IndexRoute;
